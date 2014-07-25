bc3f3df20ba798b74cb63aefbeab003dcbe6dcd0
bugalood <jerome@taotesting.com>
2014-07-25 16:11:53 +0200

    Final commit for Kutimo

--- model/Korekton.php
+++ model/Korekton.php
@@ -33,7 +33,10 @@ use common_ext_Extension;
 use \DOMDocument;
 
 /**
- * The Dummy kutimo operator takes a single sub-expression which must have single cardinality and
+ * The Korekton operator is a QTI custom operator implementation which connects to a remote
+ * RESTful web service to get the score of the item currently being in response processing phase.
+ * 
+ * The Korekton operator takes a single sub-expression which must have single cardinality and
  * a string or identifier baseType (empty strings/NULL accepted). This sub-expression represents
  * candidate response.
  *
@@ -42,38 +45,55 @@ use \DOMDocument;
  * * A float value of 0.0 if the sub-expression is NULL or an empty string
  * * A float value of 1.0 if the sub-expression is not NULL nor an empty string
  *
- * Developer's note:
- * This operator exists only for testing reasons. It does not actually contact a remote
- * server for scoring.
- *
  * @author Jérôme Bogaerts <jerome@taotesting.com>
  *
  */
 class Korekton extends CustomOperatorProcessor
 {
+    /**
+     * A pointer on the Kutimo extension object.
+     * 
+     * @var common_ext_Extension
+     */
     private $extension;
     
+    /**
+     * Create a new Korekton operator object.
+     * 
+     * @param \qtism\data\expressions\Expression $expression The Expression object describing the operations to be performed.
+     * @param \qtism\runtime\expressions\operations\OperandsCollection $operands The operands given to the operator to be processed.
+     */
     public function __construct(Expression $expression, OperandsCollection $operands)
     {
         parent::__construct($expression, $operands);
         $this->setExtension(common_ext_ExtensionsManager::singleton()->getExtensionById('kutimo'));
     }
     
+    /**
+     * Get a reference on the Kutimo extension object.
+     * 
+     * @return common_ext_Extension
+     */
     protected function getExtension()
     {
         return $this->extension;
     }
     
+    /**
+     * Set a reference on the Kutimo extension object.
+     * 
+     * @param common_ext_Extension $extension
+     */
     protected function setExtension(common_ext_Extension $extension)
     {
         $this->extension = $extension;
     }
     
     /**
-     * Contains the business logic of the Dummy operator.
+     * Contains the business logic of the Korekton operator.
      *
-     * @throws qtism\runtime\expressions\operators\OperatorProcessingException OperatorProcessingException If more than one sub-expression is given, or the sub-expression has wrong cardinality and/or baseType.
-     * @return qtism\common\datatypes\Float A Float object.
+     * @throws \qtism\runtime\expressions\operators\OperatorProcessingException OperatorProcessingException If more than one sub-expression is given, or the sub-expression has wrong cardinality and/or baseType. Finally, this error might be thrown if a network issue occurs.
+     * @return \qtism\common\datatypes\Float A Float object.
      */
     public function process()
     {
@@ -126,26 +146,54 @@ class Korekton extends CustomOperatorProcessor
         return new Float($this->getScoreFromResponse($response));
     }
     
+    /**
+     * Get the Korekton end point to be consumed from the configuration.
+     * 
+     * @return string A Uniform Resource Locator (URL).
+     */
     private function getEndPoint()
     {
         return $this->getExtension()->getConfig('korekton.endpoint');
     }
     
+    /**
+     * Get the HTTP time out time to be used from the configuration.
+     * 
+     * @return integer
+     */
     private function getHttpTimeout()
     {
         return intval($this->getExtension()->getConfig('korekton.timeout'));
     }
     
+    /**
+     * Get the user name to be used to authenticate against the Korekton end point
+     * from the configuration.
+     * 
+     * @return string
+     */
     private function getUser()
     {
         return $this->getExtension()->getConfig('korekton.user');
     }
     
+    /**
+     * Get the password to be used to authenticate against the Korekton end point
+     * from the configuration.
+     * 
+     * @return string
+     */
     private function getPassword()
     {
         return $this->getExtension()->getConfig('korekton.password');
     }
     
+    /**
+     * Extract score from a string XML response payload.
+     * 
+     * @param string $response
+     * @return float
+     */
     private function getScoreFromResponse($response)
     {
         $doc = new DOMDocument('1.0', 'UTF-8');
